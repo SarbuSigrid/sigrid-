b7e2feee022c3179d5d40c9a7984d9803442f380
JavaScript <javascript@Admins-iMac-62.local>
2020-01-14 20:56:42 +0200

    hang.

--- exercitii /hangman/hang.js
+++ exercitii /hangman/hang.js
@@ -1,4 +1,4 @@
- ar game= {
+ var game= {
      words: ['apple','orange','banana'],
      word:undefined,
      letters:[],
@@ -12,15 +12,46 @@
      },
      tryLetter:function(letter) {
          this.letters.push(letter);
-         if  (!this.word.split('').includes(letter)){
+         if  (!this.word.includes(letter)){
               this.triesLeft--;
          }
      },
     isDead:function() {
-        rturn this.triesLeft== 0;
+        return this.triesLeft== 0;
     },
     
-    isWinner:function(){
-
+    isWinner: function(){
+        return this.word.split('')
+                 .every(letter => this.letters.includes(letter))
     }
  }
+ function renderLetters () {
+     document.getElementById("letters").innerText =
+             game.letters.join(' ');
+ }
+ function renderWord () {
+     let text = game.word.split('')
+         .map(letter =>  game.letters.includes(letter) ? letter :'_')
+             .join(' ');
+    document.getElementById("word").innerText =text;
+  
+ }
+ function renderDrawing () {
+     let parts = ['head','body','left_arm','right_arm','left_leg','right_leg'];
+     let visible = parts.slice(0,6 - game.triesLeft);
+     let invisible =parts.slice (6 - game.triesLeft);
+     visible.forEach(name => document.getElementById(name).style.visibility='visible');
+ }
+ function renderView( ) {
+    renderLetters();
+    renderWord();
+    renderDrawing();
+ }
+
+ window.addEventListener('keydown',function(event) {
+     game.tryLetter(event.key) ;
+     renderView;
+ });
+    
+ game.start();
+ renderView();
